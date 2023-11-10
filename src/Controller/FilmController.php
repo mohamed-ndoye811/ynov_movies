39cee2694aab7fad031244568490fb18536d3183
Iannis Vasselet <iannis.taioglou@gmail.com>
2023-11-10 17:04:40 +0100

    Controller fix

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -16,4 +16,28 @@ class FilmController extends AbstractController
             'path' => 'src/Controller/FilmController.php',
         ]);
     }
+    
+    #[Route('/movies', name: 'movies_listing')]
+    public function list(): Response
+    {
+        return $this->json(['movies' => []]);
+    }
+
+    #[Route('/movies/{id}', name: 'get_movie')]
+    public function getMovie(int $id): Response
+    {
+        return $this->json(['movies' => "Movie $id"]);
+    }
+
+    #[Route('/movies/{id}', name: 'edit_movie')]
+    public function editMovie(int $id): Response
+    {
+        return $this->json(['created_movie' => "Movie $id"]);
+    }
+
+    #[Route('/movies/{id}', name: 'delete_movie')]
+    public function deleteMovie(int $id): Response
+    {
+        return $this->json(['deleted_movie' => "Movie $id"], 204);
+    }
 }
