3b1853922b8512bab7c17f3243201386a5ce7893
Mohamed Ndoye <NdoyeMD@outlook.fr>
2023-11-30 11:57:55 +0100

    update docker compose & update fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -3,31 +3,24 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
-use App\Entity\Film;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class AppFixtures extends Fixture
+class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-        $categories = [];
         for ($i = 0; $i < 12; $i++) {
             $category = new Category();
-
             $category->setName($faker->word());
-
             $manager->persist($category);
-            $manager->flush();
-            $categories[] = $category;
 
-            $this->addReference('categ_' . $i, $category);
+            $this->addReference('categ-' . $i, $category);
         }
 
-
-
+        $manager->flush();
     }
 }
